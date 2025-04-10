831a86d68a0c3a5213a392c7eeb2306cffa96ec6
traveler <ivan.chi@gtjas.com.hk>
2025-04-10 16:22:22 +0800

    add feature:Choose Daylight saving

--- main_cmd.py
+++ main_cmd.py
@@ -11,13 +11,31 @@ def cmd_line():
     os.system('start cmd /k "cmd_app.py"')  # direct execute the code with showing directory first
     os.system("taskkill /f /im cmd.exe") # it will close cmd when nothing is running, but main_app2.py keep looping = will not close, need a app.py out of loop, loop outside
 
-
-schedule.every().tuesday.at("06:01").do(cmd_line)
-schedule.every().wednesday.at("06:01").do(cmd_line)
-schedule.every().thursday.at("06:01").do(cmd_line)
-schedule.every().friday.at("06:01").do(cmd_line)
-schedule.every().saturday.at("06:01").do(cmd_line)
 #os.system("start cmd") # directory start at where this .py locate, that is C:\Users\Ichi\Desktop\quick_py\partial>
+
+def get_season_input():
+    while True:
+        season = input("Summer/Winter?")
+        if season in ["Summer", "Winter"]:
+            return season
+        else:
+            print("Invalid input. Please enter 'Summer' or 'Winter'.")
+
+season = get_season_input()
+
+if season == "Summer":
+    schedule.every().tuesday.at("05:01").do(cmd_line)
+    schedule.every().wednesday.at("05:01").do(cmd_line)
+    schedule.every().thursday.at("05:01").do(cmd_line)
+    schedule.every().friday.at("05:01").do(cmd_line)
+    schedule.every().saturday.at("05:01").do(cmd_line)
+elif season == "Winter":
+    schedule.every().tuesday.at("06:01").do(cmd_line)
+    schedule.every().wednesday.at("06:01").do(cmd_line)
+    schedule.every().thursday.at("06:01").do(cmd_line)
+    schedule.every().friday.at("06:01").do(cmd_line)
+    schedule.every().saturday.at("06:01").do(cmd_line)
+
 
 print("Scheduler started. Waiting for the scheduled time...")
 
