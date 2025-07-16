a4856de827897ca107f6807bbb7ba28b027df8e0
traveler <ivan.chi@gtjas.com.hk>
2025-07-16 12:26:47 +0800

    update fxdr_to_csv.py to handle user opened excel file source

--- fxdr_to_csv.py
+++ fxdr_to_csv.py
@@ -13,7 +13,7 @@ def refresh_file(file):
     wb =  xlapp.Workbooks.Open(path)
     wb.RefreshAll()
     xlapp.CalculateUntilAsyncqueriesDone()
-    wb.Save()
+    wb.SaveAs(path.replace(".xlsm", "_refreshed_temp.xlsm"))
     xlapp.Quit()
 
 def load_bal_data(file_path):
@@ -23,8 +23,9 @@ def load_bal_data(file_path):
             'Our Ref': str,
             'Account No.': str
             }
-        data = pd.read_excel(file_path, sheet_name="Deal", dtype=dtype_spec)  # specific setting for FOCCP file # without note++ edit, the ClientID remain obj ??
-        # .read_excel -> ac no. 123555 -> 123555.0 here maybe
+        refreshed_path = file_path.replace(".xlsm", "_refreshed_temp.xlsm")
+        data = pd.read_excel(refreshed_path, sheet_name="Deal", dtype=dtype_spec)  # specific setting for FOCCP file # without note++ edit, the ClientID remain obj ??
+        os.remove(refreshed_path) 
         logging.info(f"Data loaded successfully from {file_path}")
         return data
     except Exception as e:
@@ -34,10 +35,11 @@ def load_bal_data(file_path):
 
 def basic_run():
 
-    df = load_bal_data(fxdr_path) # ac no. 123555 become 123555.0 here into df
-
-    df = df.drop(df.iloc[:, 28:55],axis = 1)
-    df = df.dropna(axis=0, thresh=2) # thresh=1, then at least have 1 non-nan'row keep, as a result all row keep; but thresh=2, do not meet 2 non-nan value's row drop, = what I want
-
-    df.to_csv('C:\\Users\\Ichi\\Desktop\\quick_py\\partial\\fxdr\\current_deal.csv', encoding='utf-8', index=False, quoting=csv.QUOTE_ALL)
-
+    try:
+        df = load_bal_data(fxdr_path) # ac no. 123555 become 123555.0 here into df
+        df = df.drop(df.iloc[:, 28:55],axis = 1)
+        df = df.dropna(axis=0, thresh=2) # thresh=1, then at least have 1 non-nan'row keep, as a result all row keep; but thresh=2, do not meet 2 non-nan value's row drop, = what I want
+        df.to_csv('C:\\Users\\Ichi\\Desktop\\quick_py\\partial\\fxdr\\current_deal.csv', encoding='utf-8', index=False, quoting=csv.QUOTE_ALL)
+    except Exception as e:
+        logging.error(f"Error in basic_run: {e}")
+        raise
